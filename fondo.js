d3a306226cd7358a7343a3bc9fc7e09f54ad29d1
Francho Garcia <franchog79@gmail.com>
2019-06-15 10:41:17 +0200

    first commit

---
+++ fondo.js
@@ -0,0 +1,98 @@
+class Fondo {
+    constructor(backgX, backgY, backgWidth, backHeight, pathImage) {
+        this.canvasDOMEl = undefined;
+        /** @type CanvasRenderingContext2D */
+        this.ctx = undefined;
+        this.backgX = backgX;
+        this.backgY = backgY;
+        this.backgWidth = backgWidth;
+        this.backHeight = backHeight;
+        this.backgroundPos = 0;
+        this.pathImage = pathImage;
+        this.Backgrounds = [{
+                imagen: new Image(),
+                x: 0,
+                y: 0,
+                counterX: 0
+            },
+            {
+                imagen: new Image(),
+                x: 0,
+                y: 0,
+                counterX: 0
+            }
+        ];
+        this.intervalID = undefined;
+    }
+
+    init() {
+        /** @type HTMLCanvasElement */
+        this.canvasDOMEl = document.getElementById("myCanvas");
+        /** @type CanvasRenderingContext2D */
+        this.ctx = this.canvasDOMEl.getContext("2d");
+        this.canvasDOMEl.width = this.backgWidth;
+        this.canvasDOMEl.height = this.backHeight;
+        this.backgroundPos = this.backgX + this.backgWidth;
+        for (let cont = 0; cont < 2; cont++) {
+            if (cont == 1) {
+                this.Backgrounds[cont].x = this.backgWidth;
+            }
+            this.Backgrounds[cont].imagen.onload = () => {
+                this.ctx.drawImage(
+                    this.Backgrounds[cont].imagen,
+                    this.Backgrounds[cont].x,
+                    this.Backgrounds[cont].y,
+                    this.backgWidth,
+                    this.backHeight
+                );
+            };
+            this.Backgrounds[cont].imagen.src = this.pathImage;
+        }
+    }
+
+    drawBackground = () => {
+        let img = new Image();
+        img.src = this.pathImage;
+        // debugger;
+        this.Backgrounds[0].x = this.Backgrounds[0].x - 1;
+        this.Backgrounds[1].x = this.Backgrounds[1].x - 1;
+
+        if (this.backgroundPos > 0) {
+            if (this.Backgrounds[0].x < this.Backgrounds[1].x) {
+                this.backgroundPos = this.Backgrounds[0].x + this.backgWidth;
+            } else {
+                this.backgroundPos = this.Backgrounds[1].x + this.backgWidth;
+            }
+        } else {
+            if (this.Backgrounds[0].x < this.Backgrounds[1].x) {
+                this.backgroundPos = this.Backgrounds[1].x + this.backgWidth;
+                this.Backgrounds[0].x = this.Backgrounds[1].x + this.backgWidth;
+            } else {
+                this.backgroundPos = this.Backgrounds[0].x + this.backgWidth;
+                this.Backgrounds[1].x = this.Backgrounds[0].x + this.backgWidth;
+            }
+        }
+
+        this.ctx.drawImage(
+            img,
+            this.Backgrounds[0].x,
+            this.Backgrounds[0].y,
+            this.backgWidth,
+            this.backHeight
+        );
+        this.ctx.drawImage(
+            img,
+            this.Backgrounds[1].x,
+            this.Backgrounds[1].y,
+            this.backgWidth,
+            this.backHeight
+        );
+    };
+
+    runAnimation() {
+        this.intervalID = setInterval(() => {
+            this.ctx.clearRect(0, 0, this.backgWidth, this.backHeight);
+            this.drawBackground();
+        }, 5);
+    }
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+
+    let fondoCanvas = new Fondo(0, 0, 700, 500, "./fondo.png")
+    fondoCanvas.init()
+    fondoCanvas.runAnimation()
+}
